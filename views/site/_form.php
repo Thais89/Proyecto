fa3ea35ab20ca3a671099dea0a2ec017292f819c
Julio Carreño <jcpware@gmail.com>
2016-04-05 21:17:03 -0430

    Se cambió el formulario de registro a dos columnas

--- views/site/_form.php
+++ views/site/_form.php
@@ -8,28 +8,33 @@ use yii\bootstrap\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="usuarios-form">
+<?php $form = ActiveForm::begin(); ?>
+<div class="usuarios-form row">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <div class="col-sm-12 col-md-6">
+        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>    
+        <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'apellido')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'cedula')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'direccion')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'telefono')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group text-center">
-        <?= Html::submitButton($model->isNewRecord ? 'Crear Cuenta' : 'Actualizar', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-primary']) ?>
+        <?= $form->field($model, 'apellido')->textInput(['maxlength' => true]) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <div class="col-sm-12 col-md-6">
+        <?= $form->field($model, 'cedula')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'direccion')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'telefono')->textInput(['maxlength' => true]) ?>
+    </div>
+    
+    <div class="row">
+    <div class="col-sm-12 col-md-12">
+        <div class="form-group text-center">
+        <?= Html::submitButton($model->isNewRecord ? 'Crear Cuenta' : 'Actualizar', ['class' => $model->isNewRecord ? 'btn btn-primary' : 'btn btn-primary']) ?>
+        </div>
+    </div>
+    </div>
 
 </div>
+<?php ActiveForm::end(); ?>
--- views/site/registro-usuario.php
+++ views/site/registro-usuario.php
@@ -12,7 +12,7 @@ $this->title = 'Registro de Usuarios';
 <div class="usuarios-create container">
 <div class="row">
 
-<div class="col-sm-12 col-md-8 col-md-offset-2">
+<div class="col-sm-12 col-md-10 col-md-offset-1">
     <h1 align="center"><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
